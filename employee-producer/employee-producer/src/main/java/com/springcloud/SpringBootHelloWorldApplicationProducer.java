44cc0b9072a5cd16dcf290b14c9081249b1013f7
sivaprakashgorantla <sivaprakashgorantla@gmail.com>
2019-08-09 22:44:16 +0530

    Add files via upload

---
+++ employee-producer/employee-producer/src/main/java/com/springcloud/SpringBootHelloWorldApplicationProducer.java
@@ -0,0 +1,13 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplicationProducer {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplicationProducer.class, args);
+	}
+}
